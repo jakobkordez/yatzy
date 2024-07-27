71c6c2bb40e067358b9ee936e36a4afe906dde94
Jakob Kordež <jakobkordez1999@gmail.com>
2024-07-27 08:59:22 +0200

    Update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import themes from 'daisyui/src/theming/themes'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,svelte,js,ts}'],
@@ -6,7 +8,12 @@ export default {
 	},
 	plugins: [require('daisyui')],
 	daisyui: {
-		themes: ['cupcake'],
+		themes: [{
+			light: {
+				...themes['cupcake'],
+				"--rounded-btn": "0.5rem"
+			}
+		}],
 		logs: false
 	}
 };
