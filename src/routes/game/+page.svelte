525d387a04f7ba31e0fcfaa26fbce0b86c1a5cdc
Jakob Kordež <jakobkordez1999@gmail.com>
2024-07-28 12:04:38 +0200

    Fix svg size error and layout

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -4,7 +4,7 @@
 	import Inputs from './inputs.svelte';
 </script>
 
-<div class="flex flex-col items-center gap-10 md:flex-row md:items-start">
+<div class="flex flex-col items-center gap-10 md:flex-row md:items-start md:justify-center">
 	<div class="flex min-w-64 max-w-md flex-1 flex-col gap-6">
 		<QuitButton />
 
